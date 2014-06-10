91f9de246bf1b5046841b98af5d36e4062769a89
Mikhail Gusarov <dottedmag@dottedmag.net>
2014-06-10 17:19:59 +0200

    Stop customizing browse-url-*, defaults work fine

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -62,8 +62,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(browse-url-browser-function (quote browse-url-generic))
- '(browse-url-generic-program "google-chrome")
  '(canlock-password "fb91c0c9c555c5d0f79559ce2bfa8474922896af")
  '(safe-local-variable-values
    (quote
