7fefa71c69eed3769f5aceb56bdbc26e6ecd055a
Mikhail Gusarov <dottedmag@dottedmag.net>
2014-12-09 21:02:32 +0100

    Install necessary el-get packages by default

--- .emacs.d/el-get.el
+++ .emacs.d/el-get.el
@@ -8,4 +8,13 @@
       (goto-char (point-max))
       (eval-print-last-sexp))))
 
-(el-get 'sync)
+(setq my-packages '(ample-regexps auto-highlight-symbol cider
+      cl-lib clojure-mode d-mode dash dynamic-fonts edts el-get
+      emacs-goodies-el epl etags-select exec-path-from-shell f
+      flex-mode flycheck font-utils fringe-helper git-gutter
+      git-gutter-fringe git-modes go-mode guide-key haskell-mode
+      haskell-mode-exts list-utils lua-mode magit markdown-mode
+      org-mode package pcache persistent-soft pkg-info popwin
+      queue rnc-mode s ucs-utils unicode-fonts yaml-mode))
+
+(el-get 'sync my-packages)
