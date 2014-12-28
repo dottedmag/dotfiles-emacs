1dfedaf01b4bb5c615987f03201547b277b3b3c1
Mikhail Gusarov <dottedmag@dottedmag.net>
2014-12-28 22:39:29 +0100

    Remove cruft
    Gone are:
     - Gnus
     - BBDB
     - Vala mode
     - Dart mode
     - Edje mode & Enlightenment code style

--- .emacs.d/bbdb.el
+++
@@ -1,9 +0,0 @@
-;; -*- emacs-lisp -*-
-
-(if (require 'bbdb "bbdb" 'please-do-not-fail)
-  (progn
-    (bbdb-initialize 'gnus 'message 'sc)
-    (setq bbdb-file "~/.org/bbdb"
-	  bbdb-north-american-phone-numbers-p nil
-	  bbdb-offer-save 'yes-please-save-all-files-automatically
-	  bbdb-use-pop-up nil)))
--- .emacs.d/vala.el
+++
@@ -1,3 +0,0 @@
-(autoload 'vala-mode "vala-mode" "Major mode for editing Vala code." t)
-(add-to-list 'auto-mode-alist '("\\.vala$" . vala-mode))
-(add-to-list 'auto-mode-alist '("\\.vapi$" . vala-mode))
