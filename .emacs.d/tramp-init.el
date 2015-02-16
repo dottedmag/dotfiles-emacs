2736fd7766c28b9ec0cec2355c9b8021a7a3a2cc
Mikhail Gusarov <dottedmag@dottedmag.net>
2015-02-16 22:49:58 +0100

    Fix tramp connections to root@dottedmag.net

--- .emacs.d/tramp-init.el
+++ .emacs.d/tramp-init.el
@@ -2,3 +2,8 @@
 
 (setq tramp-persistency-file-name "~/.var-emacs/tramp")
 (require 'tramp)
+
+(setq tramp-ssh-controlmaster-options
+      "-o ControlPath=%%C -o ControlMaster=auto -o ControlPersist=no")
+(add-to-list 'tramp-default-proxies-alist
+             '("dottedmag\\.net\\'" "\\`root\\'" "/ssh:%h:"))
