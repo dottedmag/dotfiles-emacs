8d0778aa263fae54424be74aea072d91ef38dc8c
Gusarovdottedmag"@dottedmag.net <Gusarovdottedmag"@dottedmag.net>
2015-03-18 14:28:52 +0100

    Add vu/vu32 as a sudo-trampoline

--- .emacs.d/tramp-init.el
+++ .emacs.d/tramp-init.el
@@ -7,3 +7,7 @@
       "-o ControlPath=%%C -o ControlMaster=auto -o ControlPersist=no")
 (add-to-list 'tramp-default-proxies-alist
              '("dottedmag\\.net\\'" "\\`root\\'" "/ssh:%h:"))
+(add-to-list 'tramp-default-proxies-alist
+             '("vu\\'" "\\`root\\'" "/ssh:%h:"))
+(add-to-list 'tramp-default-proxies-alist
+             '("vu32\\'" "\\`root\\'" "/ssh:%h:"))
