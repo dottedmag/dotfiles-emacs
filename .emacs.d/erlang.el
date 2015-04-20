e5bdd5bbece0b171703c2c8fc6c7fcc7958bcdc7
Mikhail Gusarov <dottedmag@dottedmag.net>
2015-04-20 08:45:53 +0200

    Disable erlang snippet

--- .emacs.d/erlang.el
+++ .emacs.d/erlang.el
@@ -1,13 +1,13 @@
-(when (eq system-type 'darwin)
-  (when (file-exists-p "/usr/local/lib/erlang")
-    (setq e-path "/usr/local/lib/erlang"
-	  load-path (cons (concat e-path "/lib/tools-2.7.1/emacs/") load-path)
-	  erlang-root-dir e-path
-	  exec-path (cons (concat e-path "/bin") exec-path))
-    (require 'erlang-start)))
+;; (when (eq system-type 'darwin)
+;;   (when (file-exists-p "/usr/local/lib/erlang")
+;;     (setq e-path "/usr/local/lib/erlang"
+;; 	  load-path (cons (concat e-path "/lib/tools-2.7.1/emacs/") load-path)
+;; 	  erlang-root-dir e-path
+;; 	  exec-path (cons (concat e-path "/bin") exec-path))
+;;     (require 'erlang-start)))
 
-(defun my-erlang-mode-hook ()
-  (local-set-key [?\M-q] 'execute-extended-command)
-  (local-set-key [?\M-x] 'erlang-fill-paragraph))
+;; (defun my-erlang-mode-hook ()
+;;   (local-set-key [?\M-q] 'execute-extended-command)
+;;   (local-set-key [?\M-x] 'erlang-fill-paragraph))
 
-(add-hook 'erlang-mode-hook 'my-erlang-mode-hook)
+;; (add-hook 'erlang-mode-hook 'my-erlang-mode-hook)
