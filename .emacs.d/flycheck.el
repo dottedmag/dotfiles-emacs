2f34419447663c3ff30c6dead6b659bcfbf6cdfe
Mikhail Gusarov <dottedmag@dottedmag.net>
2015-09-01 00:18:57 +0200

    Work around conflict between popup and whitespace-mode

--- .emacs.d/flycheck.el
+++ .emacs.d/flycheck.el
@@ -3,6 +3,17 @@
 (setq flycheck-check-syntax-automatically '(save idle-change mode-enabled)
       flycheck-idle-change-delay 5)
 
+;; popup does not work well with whitespace-mode, so disable ws-mode while
+;; viewing a popup in flycheck.
+
+(defun dm>flycheck-pos-tip-error-messages (errors)
+  (let ((ws-mode global-whitespace-mode))
+    (if ws-mode
+        (global-whitespace-mode -1))
+    (flycheck-pos-tip-error-messages errors)
+    (if ws-mode
+        (global-whitespace-mode))))
+
 (eval-after-load 'flycheck
   '(custom-set-variables
-   '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
+   '(flycheck-display-errors-function #'dm>flycheck-pos-tip-error-messages)))
