2107513222b03ec5e6897c1e42d10d7426e548ba
Mikhail Gusarov <dottedmag@dottedmag.net>
2015-10-11 11:06:58 +0200

    Add .js6 to list of JavaScript files

--- .emacs.d/lisp/dm-javascript.el
+++ .emacs.d/lisp/dm-javascript.el
@@ -1,5 +1,6 @@
 (require 'js)
 
+(add-to-list 'auto-mode-alist '("\\.js6\\'" . javascript-mode))
 (add-to-list 'interpreter-mode-alist '("node" . js-mode))
 
 (define-key js-mode-map "\C-m" 'newline-and-indent)
