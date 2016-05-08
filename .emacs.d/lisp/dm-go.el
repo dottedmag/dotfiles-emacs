3a3302b95f5fbe552e5ded7d8bfb2e16969de15e
Mikhail Gusarov <dottedmag@dottedmag.net>
2016-05-08 10:59:13 +0200

    Fix wrong `add-to-list' invocation

--- .emacs.d/lisp/dm-go.el
+++ .emacs.d/lisp/dm-go.el
@@ -15,8 +15,9 @@
   (make-local-variable 'whitespace-line-column)
   (setq whitespace-line-column 120)
   (make-local-variable 'whitespace-style)
-  (add-to-list 'whitespace-style 'indentation::tab 'space-after-tab::tab
-                  'space-before-tab::tab))
+  (add-to-list 'whitespace-style 'indentation::tab)
+  (add-to-list 'whitespace-style 'space-after-tab::tab)
+  (add-to-list 'whitespace-style 'space-before-tab::tab))
 
 (defun dm-go-mode-hook ()
   "Setup `go-mode'."
