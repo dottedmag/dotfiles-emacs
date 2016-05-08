0b3371a7647429990910c81a71ef52c15d94374e
Mikhail Gusarov <dottedmag@dottedmag.net>
2016-05-08 10:49:47 +0200

    Fix Elisp coding conventions

--- .emacs.d/lisp/dm-whitespace.el
+++ .emacs.d/lisp/dm-whitespace.el
@@ -1,3 +1,9 @@
+;;; dm-whitespace -- Default `whitespace-mode' settings
+
+;;; Commentary:
+
+;;; Code:
+
 (require 'whitespace)
 
 (setq whitespace-global-modes '(not dired-mode org-mode))
@@ -5,3 +11,6 @@
 (setq whitespace-style '(face trailing tabs lines-tail empty tab-mark))
 
 (global-whitespace-mode t)
+
+(provide 'dm-whitespace)
+;;; dm-whitespace.el ends here
