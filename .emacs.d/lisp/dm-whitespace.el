a8d2cb6128479e5e3cf59e8459bd85841e871821
Mikhail Gusarov <dottedmag@dottedmag.net>
2016-12-19 18:57:25 +0100

    only enable whitespace-mode in $HOME and outside $HOME/srcs

--- .emacs.d/lisp/dm-whitespace.el
+++ .emacs.d/lisp/dm-whitespace.el
@@ -6,11 +6,18 @@
 
 (require 'whitespace)
 
-(setq whitespace-global-modes '(not dired-mode org-mode))
+(defun dm-enable-whitespace-mode ()
+  "Enables whitespace mode in $HOME, except ~/srcs."
+  (let* ((home (expand-file-name "~"))
+         (srcs (file-name-as-directory
+                (concat (file-name-as-directory home) "srcs"))))
+    (if (and (string-prefix-p home buffer-file-name)
+             (not (string-prefix-p srcs buffer-file-name)))
+        (whitespace-mode))))
+
+(add-hook 'find-file-hook #'dm-enable-whitespace-mode)
 
 (setq whitespace-style '(face trailing tabs lines-tail empty tab-mark))
-
-(global-whitespace-mode t)
 
 (provide 'dm-whitespace)
 ;;; dm-whitespace.el ends here
