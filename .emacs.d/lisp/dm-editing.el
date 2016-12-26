fae49979c924ed4343fa9e8d398a978600dcbb89
Mikhail Gusarov <dottedmag@dottedmag.net>
2016-12-26 21:31:14 +0100

    Fix the "fix" disabling use-notify for global-auto-revert

--- .emacs.d/lisp/dm-editing.el
+++ .emacs.d/lisp/dm-editing.el
@@ -30,6 +30,41 @@
 (global-set-key (kbd "S-C-n") 'shrink-window)
 (global-set-key (kbd "S-C-p") 'enlarge-window)
 
+;;; A copy global-auto-revert-mode with a "fix" removed:
+;;; use notification services.
+
+;;;###autoload
+(define-minor-mode global-auto-revert-mode
+  "Toggle Global Auto Revert mode.
+With a prefix argument ARG, enable Global Auto Revert mode if ARG
+is positive, and disable it otherwise.  If called from Lisp,
+enable the mode if ARG is omitted or nil.
+
+Global Auto Revert mode is a global minor mode that reverts any
+buffer associated with a file when the file changes on disk.  Use
+`auto-revert-mode' to revert a particular buffer.
+
+If `global-auto-revert-non-file-buffers' is non-nil, this mode
+may also revert some non-file buffers, as described in the
+documentation of that variable.  It ignores buffers with modes
+matching `global-auto-revert-ignore-modes', and buffers with a
+non-nil vale of `global-auto-revert-ignore-buffer'.
+
+When a buffer is reverted, a message is generated.  This can be
+suppressed by setting `auto-revert-verbose' to nil.
+
+This function calls the hook `global-auto-revert-mode-hook'.
+It displays the text that `global-auto-revert-mode-text'
+specifies in the mode line."
+  :global t :group 'auto-revert :lighter global-auto-revert-mode-text
+  (auto-revert-set-timer)
+  (if global-auto-revert-mode
+      (auto-revert-buffers)
+    (dolist (buf (buffer-list))
+      (with-current-buffer buf
+        (when auto-revert-use-notify
+          (auto-revert-notify-rm-watch))))))
+
 (global-auto-revert-mode t)
 
 (if (eq system-type 'darwin)
