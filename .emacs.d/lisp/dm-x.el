36c8a264c63892c63016f57c4d9c153ce26efc9a
Mikhail Gusarov <dottedmag@dottedmag.net>
2017-01-04 23:35:04 +0100

    Start using common OS X key combinations.

--- .emacs.d/lisp/dm-x.el
+++ .emacs.d/lisp/dm-x.el
@@ -8,23 +8,49 @@
       frame-title-format "%b - emacs")
 (mouse-avoidance-mode 'exile)
 
-(if (eq system-type 'darwin)
-    (progn
-      (setq exec-path (cons (expand-file-name "~/bin") exec-path))
-      (defun x ()
-        "Run iTerm in current directory"
-        (interactive)
-        (start-process "iterm" nil "runiterm" default-directory)))
-  (defun x ()
-    "Run X terminal in current directory"
-    (interactive)
-    (start-process "shell-x" nil "x-terminal-emulator")))
+(defun dm-x-x ()
+  "Run X terminal in turrent directory."
+  (interactive)
+  (start-process "shell-x" nil "x-terminal-emulator"))
+
+(defun dm-x-setup ()
+  "Set up Emacs for Linux/X."
+  (defalias 'x 'dm-x-x))
+
+(defun dm-darwin-x ()
+  "Run iTerm in current directory."
+  (interactive)
+  (start-process "iterm" nil "runiterm" default-directory))
+
+(defun dm-darwin-raise-new-cocoa-frame (frame)
+  "Puts newly created FRAME under Cocoa into foreground."
+  (when (display-graphic-p)
+    (select-frame-set-input-focus (selected-frame))))
+
+(defun dm-darwin-new-buffer ()
+  "Create a new unnamed buffer in a current directory."
+  (interactive)
+  (let ((buf (generate-new-buffer "untitled")))
+    (switch-to-buffer buf)
+    (funcall initial-major-mode)
+    (setq buffer-offer-save t)))
+
+(defun dm-darwin-keybindings ()
+  "Set keybindings better aligned with (my understanding of) OS X."
+  (global-set-key (kbd "s-o") 'ido-find-file)
+  (global-set-key (kbd "s-t") 'dm-darwin-new-buffer))
+
+(defun dm-darwin-setup ()
+  "Set up Emacs for OS X."
+  (dm-darwin-keybindings)
+  (setq exec-path (cons (expand-file-name "~/bin") exec-path))
+  (defalias 'x 'dm-darwin-x)
+  (add-hook 'after-make-farme-functions 'dm-darwin-raise-new-cocoa-frame t))
 
 (when (eq system-type 'darwin)
-  (defun raise-new-cocoa-frame (frame)
-    (if (display-graphic-p)
-        (select-frame-set-input-focus (selected-frame))))
-  (add-hook 'after-make-frame-functions 'raise-new-cocoa-frame t))
+  (dm-darwin-setup))
+(when (eq system-type 'gnu/linux)
+  (dm-x-setup))
 
 (provide 'dm-x)
 ;;; dm-x.el ends here
