d4d8df46d3d45c764d2fc7fdf0180f0ba85cba90
Mikhail Gusarov <dottedmag@dottedmag.net>
2017-01-07 18:54:13 +0100

    more Emacs conventions fluff

--- .emacs.d/lisp/dm-smartparens.el
+++ .emacs.d/lisp/dm-smartparens.el
@@ -1,3 +1,12 @@
+;;; dm-smartparens -- Configure smartparens
+
+;;; Commentary:
+
+;;; Code:
+
 (require 'smartparens-config)
 
 (add-hook 'prog-mode-hook 'turn-on-smartparens-strict-mode)
+
+(provide 'dm-smartparens)
+;;; dm-smartparens.el ends here
