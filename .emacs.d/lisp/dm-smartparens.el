e47708c9e76da904be7f22232b8a0d6bd918cd8a
Mikhail Gusarov <dottedmag@dottedmag.net>
2017-01-17 09:52:16 +0100

    Disable smartparens in ELisp/ClojureScript modes
    These modes are served by Paredit just fine

--- .emacs.d/lisp/dm-smartparens.el
+++ .emacs.d/lisp/dm-smartparens.el
@@ -12,10 +12,10 @@
 
 (defun dm/enable-smartparens ()
   "Enable smartparens in all programming modes except Clojure."
-  (unless (eq major-mode 'clojure-mode)
+  (unless (memq major-mode '(emacs-lisp-mode clojure-mode clojurescript-mode))
     (turn-on-smartparens-strict-mode)))
 
-(add-hook 'prog-mode-hook 'dm/enable-smartparens)
+(add-hook 'prog-mode-hook #'dm/enable-smartparens)
 
 (provide 'dm-smartparens)
 ;;; dm-smartparens.el ends here
