af2033cf42d22d8d25051fa0f36e6bcd8d840905
Mikhail Gusarov <dottedmag@dottedmag.net>
2017-01-21 19:30:13 +0100

    Enable Paredit for ELisp

--- .emacs.d/lisp/dm-elisp.el
+++ .emacs.d/lisp/dm-elisp.el
@@ -6,12 +6,14 @@
 
 (require 'dm-rainbow)
 (require 'dm-flycheck)
+(require 'dm-paredit)
 
 (eval-when-compile
   (require 'flycheck))
 
 (setq flycheck-emacs-lisp-load-path 'inherit)
 (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)
 
 (provide 'dm-elisp)
 ;;; dm-elisp.el ends here
