e0b92050733624d1d822008f394ba3046314e358
Mikhail Gusarov <dottedmag@dottedmag.net>
2017-02-22 23:26:01 +0100

    remove fluff

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -62,18 +62,3 @@
  "dm-x"
  "dm-xml"
  "dm-yaml")
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(canlock-password "fb91c0c9c555c5d0f79559ce2bfa8474922896af")
- '(send-mail-function (quote sendmail-send-it)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(put 'upcase-region 'disabled nil)
