3a7798b1a1dba3b33bff693a3102e18c3c32b56d
Mikhail Gusarov <dottedmag@dottedmag.net>
2017-03-17 17:36:02 +0100

    Install debian-changelog-mode.

--- .emacs.d/lisp/dm-debian.el
+++ .emacs.d/lisp/dm-debian.el
@@ -1,2 +1,17 @@
+;;; dm-debian -- Configuration for Debian development
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'dm-el-get)
+
+(dm-el-get 'debian-changelog-mode)
+
+(require 'debian-changelog-mode)
+
 (setq debian-changelog-full-name "Mikhail Gusarov")
 (setq debian-changelog-mailing-address "dottedmag@debian.org")
+
+(provide 'dm-debian)
+;;; dm-debian.el ends here
