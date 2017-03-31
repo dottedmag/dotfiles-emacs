abda677a6a7388fb602f301d32bc45ac743b1a1c
Mikhail Gusarov <dottedmag@dottedmag.net>
2017-03-31 16:43:12 +0200

    Do not litter ~/.emacs.d with temporary files from Projectile

--- .emacs.d/lisp/dm-projectile.el
+++ .emacs.d/lisp/dm-projectile.el
@@ -8,5 +8,10 @@
 
 (dm-el-get 'projectile)
 
+(setq-default projectile-cache-file
+              (expand-file-name "~/.var-emacs/projectile.cache"))
+(setq-default projectile-known-projects-file
+              (expand-file-name "~/.var-emacs/projectile-bookmarks.eld"))
+
 (provide 'dm-projectile)
 ;;; dm-projectile.el ends here
