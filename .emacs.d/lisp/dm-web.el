92004860a5ad2f56a5e87166b599d7c6c2945d75
Mikhail Gusarov <dottedmag@dottedmag.net>
2017-03-31 16:33:44 +0200

    Move misplaced web-mode ext association to dm-web.el

--- .emacs.d/lisp/dm-web.el
+++ .emacs.d/lisp/dm-web.el
@@ -11,6 +11,8 @@
 (require 'web-mode)
 
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+
 (setq-default web-mode-markup-indent-offset 2)
 
 (provide 'dm-web)
--- .emacs.d/lisp/dm-yaml.el
+++ .emacs.d/lisp/dm-yaml.el
@@ -8,7 +8,5 @@
 
 (dm-el-get 'yaml-mode)
 
-(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
-
 (provide 'dm-yaml)
 ;;; dm-yaml.el ends here
