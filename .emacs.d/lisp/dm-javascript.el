9aa2010b26270061a37b495b316c6f40b35b1329
Mikhail Gusarov <mikhail@hola.org>
2017-06-09 23:00:27 +0200

    configure js2-mode

--- .emacs.d/lisp/dm-javascript.el
+++ .emacs.d/lisp/dm-javascript.el
@@ -8,8 +8,26 @@
 
 (dm-el-get 'js2-mode 'rjsx-mode)
 
+(require 'js2-mode)
+
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
+
+(defun dm-add-externs (externs)
+  "Add number of EXTERNS to js2 externs list."
+  (dolist (elt externs)
+    (push elt js2-additional-externs)))
+
+(defun dm-js2-init-externs ()
+  "Add externs apropriate to jslint mode for the file."
+  (save-excursion
+    (when (re-search-forward "node:true" 1024 t)
+      (dm-add-externs js2-node-externs))))
+
+(add-hook 'js2-init-hook #'dm-js2-init-externs)
+
+(setq-default js2-strict-inconsistent-return-warning nil
+              js2-strict-trailing-comma-warning nil)
 
 (provide 'dm-javascript)
 ;;; dm-javascript.el ends here
