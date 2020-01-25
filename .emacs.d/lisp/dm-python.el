6b1f50a4716e24b4ef7d314a3e7a8922702d2b6a
Mikhail Gusarov <dottedmag@dottedmag.net>
2020-01-25 17:29:22 +0100

    Add blacken

--- .emacs.d/lisp/dm-python.el
+++ .emacs.d/lisp/dm-python.el
@@ -1,8 +1,13 @@
 (require 'python)
 
+(require 'dm-el-get)
+(dm-el-get 'blacken)
+
 ; Autoindentation after "Return"
 (define-key python-mode-map "\C-m" 'newline-and-indent)
 
 (show-trailing-whitespace-in 'python-mode-hook)
 
 (add-to-mode-alist-ext 'jinja-mode "jt")
+
+(add-hook 'python-mode-hook 'blacken-mode)
