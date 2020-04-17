230409424312d1d5722fc9457ca12d7b1e08c7be
Mikhail Gusarov <dottedmag@dottedmag.net>
2020-04-17 21:45:14 +0200

    stuff

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -4,7 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(cider-clojure-cli-global-options "-A:cider")
- '(flycheck-python-pycompile-executable "python3"))
+ '(flycheck-python-pycompile-executable "python3")
+ '(package-selected-packages (quote (csv-mode queue))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
