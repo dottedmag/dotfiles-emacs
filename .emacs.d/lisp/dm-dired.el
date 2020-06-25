2db772992447cc79d29d14cd631caaab9215c7ff
Mikhail Gusarov <dottedmag@dottedmag.net>
2020-06-25 21:21:21 +0200

    dm-dired.el upd

--- .emacs.d/lisp/dm-dired.el
+++ .emacs.d/lisp/dm-dired.el
@@ -1,3 +1,20 @@
+;;; dm-dired -- Settings for Dired
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'dired)
 (require 'dired-x)
 
 (add-to-list 'dired-omit-extensions ".lo")
+
+;; (defun dm-async-open ()
+;;   "Open file(s) under cursor using system stuff."
+;;   (interactive)
+;;   (dired-do-shell-command "open"))
+
+;; (define-key dired-mode-map (kbd "S-<return>") 'dm-async-open)
+
+(provide 'dm-dired)
+;;; dm-dired.el ends here
